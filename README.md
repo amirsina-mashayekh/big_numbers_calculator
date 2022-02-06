85ea4786203995262f5375e16c70cc220240bf4d
AmirSina Mashayekh <pajuhesh80@outlook.com>
2022-02-06 18:34:35 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Big Numbers Calculator
+
+A calculator for big numbers with some basic operations, trigonometric functions and polynomial integral.
---
+++ android/app/src/main/kotlin/ir/pajuhesh80/big_numbers_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.pajuhesh80.big_numbers_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
